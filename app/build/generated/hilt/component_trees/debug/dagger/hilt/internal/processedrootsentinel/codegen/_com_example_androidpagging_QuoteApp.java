d0907cd4824c97eba78369f04661cc9399139458
danielcerongrajales <danielcern.dcg@gmail.com>
2025-04-27 18:37:21 -0600

    update gradle v2

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_androidpagging_QuoteApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.androidpagging.QuoteApp"
-)
-public final class _com_example_androidpagging_QuoteApp {
-}
